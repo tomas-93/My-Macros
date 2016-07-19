e4ea23dd7ce27674db03bf9d8314f4bfa98d11e5
tomas-93 <tomasyussef@gmail.com>
2016-07-19 14:04:44 -0500

    Se agrega la funcionada de los metodo DAO implementado en la base de datos

--- CoreMM/src/main/java/com/mymacros/repository/database/DailyDataBaseImplementDao.java
+++ CoreMM/src/main/java/com/mymacros/repository/database/DailyDataBaseImplementDao.java
@@ -1,30 +1,53 @@
 package com.mymacros.repository.database;
 
 
-
 import com.mymacros.dao.entity.DailyDao;
 import com.mymacros.dto.entity.DailyDto;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Tomas on 18/07/2016.
  */
 public class DailyDataBaseImplementDao implements DailyDao
 {
-     public List<DailyDto> getAllDaily() {
-          return null;
+     private final Map<Long, DailyDto> dailyDtoMapDatabase = new Hashtable<Long, DailyDto>();
+     private volatile long idDaily = 1L;
+
+     public List<DailyDto> getAllDaily()
+     {
+          List<DailyDto> dailyDtoList = new ArrayList<DailyDto>();
+          for(Long id: this.dailyDtoMapDatabase.keySet())
+          {
+               dailyDtoList.add(this.dailyDtoMapDatabase.get(id));
+          }
+          return dailyDtoList;
      }
 
-     public DailyDto getDaily(long id) {
-          return null;
+     public DailyDto getDaily(long id)
+     {
+          return this.dailyDtoMapDatabase.get(id);
      }
 
-     public void createDaily(DailyDto dailyDto) {
+     public void createDaily(DailyDto dailyDto)
+     {
+          if (!dailyDtoMapDatabase.isEmpty())
+               this.idDaily ++;
 
+          dailyDto.setId(idDaily);
+          this.dailyDtoMapDatabase.put(dailyDto.getId(), dailyDto);
      }
 
-     public void deleteDaily(long id) {
+     public void deleteDaily(long id)
+     {
+          this.dailyDtoMapDatabase.remove(id);
+     }
 
+     public void updateDaily(DailyDto dailyDto)
+     {
+          this.dailyDtoMapDatabase.replace(dailyDto.getId(), dailyDto);
      }
 }
