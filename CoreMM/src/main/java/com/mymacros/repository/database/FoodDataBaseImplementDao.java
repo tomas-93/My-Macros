565dddc3b13361a77a6d6e671be91bb2e64e9033
tomas-93 <tomasyussef@gmail.com>
2016-07-19 15:55:39 -0500

    Se documenta la clase FoodDatabase

--- CoreMM/src/main/java/com/mymacros/repository/database/FoodDataBaseImplementDao.java
+++ CoreMM/src/main/java/com/mymacros/repository/database/FoodDataBaseImplementDao.java
@@ -16,6 +16,12 @@ public class FoodDataBaseImplementDao implements FoodDao
      private Map<Long, FoodDto> foodDataBase = new Hashtable<Long, FoodDto>();
      private volatile long idFood = 1L;
 
+     /**
+      * <h1>getAllFood</h1>
+      * <p>Obtiene una lista de food de la base de datos</p>
+      * @return Retorna una un objeto List que tiene una serie de objetos de la base de datos
+      *
+      */
      @Override
      public List<FoodDto> getAllFood()
      {
@@ -26,11 +32,24 @@ public class FoodDataBaseImplementDao implements FoodDao
           return list;
      }
 
+     /**
+      * <h1>getFood</h1>
+      * <p>Obtiene un dato de la base de datos</p>
+      *
+      * @param id especifica el identificador de un objeto guardado en la base de datos
+      * @return retorna el objeto especificado por el parametro id.
+      */
      @Override
      public FoodDto getFood(long id)
      {
           return this.foodDataBase.get(id);
      }
+
+     /**
+      * <h1>crearFood</h1>
+      * <p>Guarda el objeto pasado como parametro</p>
+      * @param foodDto objeto que sera guardado en la base de datos
+      */
      @Override
      public void createFood(FoodDto foodDto)
      {
@@ -41,11 +60,23 @@ public class FoodDataBaseImplementDao implements FoodDao
           this.foodDataBase.put(foodDto.getId(), foodDto);
 
      }
+
+     /**
+      * <h1>updateFood</h1>
+      * <p>Metodo que actualiza un objeto de la base de datos</p>
+      * @param foodDto es el objeto que encapsula lo datos actualizados en la base de datos
+      */
      @Override
      public void updateFood(FoodDto foodDto)
      {
           this.foodDataBase.replace(foodDto.getId(), foodDto);
      }
+
+     /**
+      * <h1>deleteFood</h1>
+      * <p>Elimina un elemento de la base de datos</p>
+      * @param id identificador el cual reprecenta el elemnto que se dea borrar
+      */
      @Override
      public void deleteFood(long id)
      {
