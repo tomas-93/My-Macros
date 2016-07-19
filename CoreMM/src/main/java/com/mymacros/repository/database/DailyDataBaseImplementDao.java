dc4eabfcdde66480cd39c0ee95732ee2e69a605f
tomas-93 <tomasyussef@gmail.com>
2016-07-19 14:16:07 -0500

    Se documenta la clase DailyDatabase

--- CoreMM/src/main/java/com/mymacros/repository/database/DailyDataBaseImplementDao.java
+++ CoreMM/src/main/java/com/mymacros/repository/database/DailyDataBaseImplementDao.java
@@ -17,6 +17,11 @@ public class DailyDataBaseImplementDao implements DailyDao
      private final Map<Long, DailyDto> dailyDtoMapDatabase = new Hashtable<Long, DailyDto>();
      private volatile long idDaily = 1L;
 
+     /**
+      * <h1>getAllDaily</h1>
+      * <p>Se obtienen todos lo usuarios de la base de datso</p>
+      * @return Retorna una lista del objecto DailyDto
+      */
      public List<DailyDto> getAllDaily()
      {
           List<DailyDto> dailyDtoList = new ArrayList<DailyDto>();
@@ -27,11 +32,22 @@ public class DailyDataBaseImplementDao implements DailyDao
           return dailyDtoList;
      }
 
+     /**
+      * <h1>getDaily</h1>
+      * <p>Se obtiene un objecto especifico de la base de datos</p>
+      * @param id Este parametro es el valor del id de objecto que se quiere obtener
+      * @return El objecto que se obtuvo de la base detos
+      */
      public DailyDto getDaily(long id)
      {
           return this.dailyDtoMapDatabase.get(id);
      }
 
+     /**
+      * <h1>createDaily</h1>
+      * <p>Se almacena un objeto DailyDto a la base de datos</p>
+      * @param dailyDto entidad que sera almacenado en la base de datos
+      */
      public void createDaily(DailyDto dailyDto)
      {
           if (!dailyDtoMapDatabase.isEmpty())
@@ -41,11 +57,21 @@ public class DailyDataBaseImplementDao implements DailyDao
           this.dailyDtoMapDatabase.put(dailyDto.getId(), dailyDto);
      }
 
+     /**
+      * <h1>deleteDaily</h1>
+      * <p>Elimina un objeto diario de la base de datos</p>
+      * @param id indetificador de la entidad que sera eliminada
+      */
      public void deleteDaily(long id)
      {
           this.dailyDtoMapDatabase.remove(id);
      }
 
+     /**
+      * <h1>updateDaily</h1>
+      * <p>Actualiza un objeto de la base de datos</p>
+      * @param dailyDto entidad que contendra los datos actualizados 
+      */
      public void updateDaily(DailyDto dailyDto)
      {
           this.dailyDtoMapDatabase.replace(dailyDto.getId(), dailyDto);
