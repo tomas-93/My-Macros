d3d91c21178b91c531e2e1b10491ee66e2de89c2
tomas-93 <tomasyussef@gmail.com>
2016-07-19 15:43:20 -0500

    Se agrega la funcionalidad de los metodos de la interface FoodDao en la clase FoodDatabase

--- CoreMM/src/main/java/com/mymacros/repository/database/FoodDataBaseImplementDao.java
+++ CoreMM/src/main/java/com/mymacros/repository/database/FoodDataBaseImplementDao.java
@@ -3,36 +3,52 @@ package com.mymacros.repository.database;
 import com.mymacros.dao.entity.FoodDao;
 import com.mymacros.dto.entity.FoodDto;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Tomas on 18/07/2016.
  */
 public class FoodDataBaseImplementDao implements FoodDao
 {
+     private Map<Long, FoodDto> foodDataBase = new Hashtable<Long, FoodDto>();
+     private volatile long idFood = 1L;
 
+     @Override
      public List<FoodDto> getAllFood()
      {
-          return null;
+          List<FoodDto> list = new ArrayList<FoodDto>();
+          for(Long id: this.foodDataBase.keySet())
+               list.add(this.foodDataBase.get(id));
+
+          return list;
      }
 
+     @Override
      public FoodDto getFood(long id)
      {
-          return null;
+          return this.foodDataBase.get(id);
      }
-
+     @Override
      public void createFood(FoodDto foodDto)
      {
+          if(!this.foodDataBase.isEmpty())
+               this.idFood++;
+
+          foodDto.setId(idFood);
+          this.foodDataBase.put(foodDto.getId(), foodDto);
 
      }
-
+     @Override
      public void updateFood(FoodDto foodDto)
      {
-
+          this.foodDataBase.replace(foodDto.getId(), foodDto);
      }
-
+     @Override
      public void deleteFood(long id)
      {
-
+          this.foodDataBase.remove(id);
      }
 }
