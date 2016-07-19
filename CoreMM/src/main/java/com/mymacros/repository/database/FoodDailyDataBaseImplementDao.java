24636ec71bbc38d320a8b175ccaca7d7394f8498
tomas-93 <tomasyussef@gmail.com>
2016-07-19 14:39:55 -0500

    Se agrega la fuincionalidad de los metodos de la interface FoodDailyDao en la clase FoodDailyDatabase

--- CoreMM/src/main/java/com/mymacros/repository/database/FoodDailyDataBaseImplementDao.java
+++ CoreMM/src/main/java/com/mymacros/repository/database/FoodDailyDataBaseImplementDao.java
@@ -3,29 +3,52 @@ package com.mymacros.repository.database;
 import com.mymacros.dao.entity.FoodDailyDao;
 import com.mymacros.dto.entity.FoodDailyDto;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Tomas on 18/07/2016.
  */
-public class FoodDailyDataBaseImplementDao implements FoodDailyDao {
-     public List<FoodDailyDto> getAllFoodDaily() {
-          return null;
+public class FoodDailyDataBaseImplementDao implements FoodDailyDao
+{
+     private final Map<Long, FoodDailyDto> foodDailyDatabase = new Hashtable<Long, FoodDailyDto>();
+     private volatile long idFoodDaily = 1L;
+     @Override
+     public List<FoodDailyDto> getAllFoodDaily()
+     {
+          List<FoodDailyDto> list = new ArrayList<FoodDailyDto>();
+          for(Long id: this.foodDailyDatabase.keySet())
+               list.add(this.foodDailyDatabase.get(id));
+          return list;
      }
 
-     public FoodDailyDto getFoodDaily(long id) {
-          return null;
+     @Override
+     public FoodDailyDto getFoodDaily(long id)
+     {
+          return this.foodDailyDatabase.get(id);
      }
 
-     public void crateFoodDaily(FoodDailyDto foodDailyDto) {
+     @Override
+     public void crateFoodDaily(FoodDailyDto foodDailyDto)
+     {
+          if(!foodDailyDatabase.isEmpty())
+               this.idFoodDaily++;
 
+          foodDailyDto.setId(idFoodDaily);
+          this.foodDailyDatabase.put(foodDailyDto.getId(), foodDailyDto);
      }
 
-     public void updateFoodDaily(FoodDailyDto foodDailyDto) {
-
+     @Override
+     public void updateFoodDaily(FoodDailyDto foodDailyDto)
+     {
+          this.foodDailyDatabase.replace(foodDailyDto.getId(), foodDailyDto);
      }
 
-     public void deleteFoodDaily(long id) {
-
+     @Override
+     public void deleteFoodDaily(long id)
+     {
+          this.foodDailyDatabase.remove(id);
      }
 }
