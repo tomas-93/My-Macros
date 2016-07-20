4b04964644b65e933667d5f74d24c5fd9795a313
tomas-93 <tomasyussef@gmail.com>
2016-07-20 10:09:19 -0500

    Se documenta la clase RecipeDataBase

--- CoreMM/src/main/java/com/mymacros/repository/database/RecipeDataBaseImplementDao.java
+++ CoreMM/src/main/java/com/mymacros/repository/database/RecipeDataBaseImplementDao.java
@@ -1,6 +1,7 @@
 package com.mymacros.repository.database;
 
 import com.mymacros.dao.entity.FoodRecipeDao;
+import com.mymacros.dao.entity.MacronutrientsDao;
 import com.mymacros.dao.entity.RecipeDao;
 import com.mymacros.dto.entity.RecipeDto;
 import org.springframework.stereotype.Repository;
@@ -20,8 +21,16 @@ public class RecipeDataBaseImplementDao implements RecipeDao
      private final Map<Long, RecipeDto> recipeDtoDatabase = new Hashtable<>();
      private volatile long idRecipe = 1L;
      @Inject
+     private MacronutrientsDao macronutrientsDao;
+     @Inject
      private FoodRecipeDao foodRecipeDao;
 
+     /**
+      * <h1>getAllRecipe</h1>
+      * <p>Obtiene una lista de los elemento de la base de datos</p>
+      *
+      * @return Lista de todos los objetos de la base de datos
+      */
      @Override
      public List<RecipeDto> getAllRecipe()
      {
@@ -31,12 +40,22 @@ public class RecipeDataBaseImplementDao implements RecipeDao
                   .collect(Collectors.toList());
      }
 
+     /**
+      * <h1>getRecipeDto</h1>
+      * <p>Obtiene un elemento de la base de datos</p>
+      * @param id Indetificador del objeto a obtener de la base de datos
+      * @return Retorna el objeto encotrnado en la base de datos
+      */
      @Override
      public RecipeDto getRecipeDto(long id)
      {
           return this.recipeDtoDatabase.get(id);
      }
-
+     /**
+      * <h1>createRecipe</h1>
+      * <p>Agrega un nuevo elemento a la base de datos</p>
+      * @param recipeDto Objeto que encapsula la informacion para alamcenar.
+      */
      @Override
      public void createRecipe(RecipeDto recipeDto)
      {
@@ -46,15 +65,29 @@ public class RecipeDataBaseImplementDao implements RecipeDao
           this.recipeDtoDatabase.put(recipeDto.getId(), recipeDto);
      }
 
+     /**
+      * <h1>updateRecipe</h1>
+      * <p>Actualiza un nuevo elemto de la base de datos</p>
+      * @param recipeDto Objeto que tiene los datos actualizados.
+      */
      @Override
      public void updateRecipe(RecipeDto recipeDto)
      {
           this.recipeDtoDatabase.replace(recipeDto.getId(), recipeDto);
      }
 
+     /**
+      * <h1>deleteRecipe</h1>
+      * <p>Elimina una receta de la base de datos, de igual forma que sus alimentos y su macros</p>
+      * @param id Indetificador que especifica elemeto a eliminar.
+      */
      @Override
      public void deleteRecipe(long id)
      {
+          final long idMacros = this.recipeDtoDatabase.get(id).getIdMacros();
+          final long idFoodRecipe = this.recipeDtoDatabase.get(id).getIdFoodRecipe();
+          this.foodRecipeDao.deleteFoodRecipe(idFoodRecipe);
+          this.macronutrientsDao.deleteMacronutrients(idMacros);
           this.recipeDtoDatabase.remove(id);
      }
 }
