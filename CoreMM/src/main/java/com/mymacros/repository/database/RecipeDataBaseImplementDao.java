f01b6841c7141aef5a8303a166d0ea49d8da9677
tomas-93 <tomasyussef@gmail.com>
2016-07-20 09:53:38 -0500

    Se implementa la funcionalidad de los metodos de la interface RecipeDao en la calse RecipeDatabase

--- CoreMM/src/main/java/com/mymacros/repository/database/RecipeDataBaseImplementDao.java
+++ CoreMM/src/main/java/com/mymacros/repository/database/RecipeDataBaseImplementDao.java
@@ -1,28 +1,60 @@
 package com.mymacros.repository.database;
 
+import com.mymacros.dao.entity.FoodRecipeDao;
 import com.mymacros.dao.entity.RecipeDao;
 import com.mymacros.dto.entity.RecipeDto;
+import org.springframework.stereotype.Repository;
 
+import javax.inject.Inject;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by Tomas on 18/07/2016.
  */
+@Repository
 public class RecipeDataBaseImplementDao implements RecipeDao
 {
-     public List<RecipeDto> getAllRecipe() {
-          return null;
+     private final Map<Long, RecipeDto> recipeDtoDatabase = new Hashtable<>();
+     private volatile long idRecipe = 1L;
+     @Inject
+     private FoodRecipeDao foodRecipeDao;
+
+     @Override
+     public List<RecipeDto> getAllRecipe()
+     {
+          return this.recipeDtoDatabase.keySet()
+                  .stream()
+                  .map(this.recipeDtoDatabase::get)
+                  .collect(Collectors.toList());
      }
 
-     public RecipeDto getRecipeDto(long id) {
-          return null;
+     @Override
+     public RecipeDto getRecipeDto(long id)
+     {
+          return this.recipeDtoDatabase.get(id);
      }
 
-     public void createRecipe(RecipeDto recipeDto) {
-
+     @Override
+     public void createRecipe(RecipeDto recipeDto)
+     {
+          if(!this.recipeDtoDatabase.isEmpty())
+               this.idRecipe++;
+          recipeDto.setId(this.idRecipe);
+          this.recipeDtoDatabase.put(recipeDto.getId(), recipeDto);
      }
 
-     public void deleteRecipe(long id) {
+     @Override
+     public void updateRecipe(RecipeDto recipeDto)
+     {
+          this.recipeDtoDatabase.replace(recipeDto.getId(), recipeDto);
+     }
 
+     @Override
+     public void deleteRecipe(long id)
+     {
+          this.recipeDtoDatabase.remove(id);
      }
 }
