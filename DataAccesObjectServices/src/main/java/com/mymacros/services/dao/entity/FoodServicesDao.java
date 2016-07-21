cfe8ea175c6f859e4eefd0a4da33e18991b108ba
tomas-93 <tomasyussef@gmail.com>
2016-07-20 23:19:23 -0500

    Este commit ->Se crea la interface MacrosServicesDao ->Se crea FoodServicesDao y sus metodos a implementar ->Se documenta la clase FoodServiceDao ->Se implementa la interface FoodServicesDao en la clase FoodServices

--- DataAccesObjectServices/src/main/java/com/mymacros/services/dao/entity/FoodServicesDao.java
+++ DataAccesObjectServices/src/main/java/com/mymacros/services/dao/entity/FoodServicesDao.java
@@ -1,7 +1,40 @@
 package com.mymacros.services.dao.entity;
 
+import com.mymacros.dto.entity.FoodDto;
+
+import java.util.List;
+
 /**
  * Created by Tomas on 20/07/2016.
  */
-public interface FoodServicesDao {
+public interface FoodServicesDao extends MacrosServicesDao
+{
+     /**
+      * <h1>getFoodDto</h1>
+      * <p>Obtiene el un objeto FoodDto del Repository</p>
+      * @param id Identificador que reprecenta el elemto a buscar
+      * @return Retorna el elemento devuelto del repositorio
+      */
+     FoodDto getFoodDto(long id);
+
+     /**
+      * <h1>getAllFoodDto</h1>
+      * <p>Obtiene una lista de todos lo elemto FoodDto del Repositorio</p>
+      * @return retorna un objeto list con todos los elemetos
+      */
+     List<FoodDto> getAllFoodDto();
+
+     /**
+      * <h1>updateFoodDto</h1>
+      * <p>Actualiza un elemento del repostorio</p>
+      * @param foodDto Objeto que encapsula los datos actualizados
+      */
+     void updateFoodDto(FoodDto foodDto);
+
+     /**
+      * <h1>deleteFoodDto</h1>
+      * <p>Elimina un elemeto del respositorio</p>
+      * @param id Identificador que reprecenta el elemento a eliminar.
+      */
+     void deleteFoodDto(long id);
 }
--- DataAccesObjectServices/src/main/java/com/mymacros/services/dao/entity/RecipeServicesDao.java
+++ DataAccesObjectServices/src/main/java/com/mymacros/services/dao/entity/RecipeServicesDao.java
@@ -1,7 +1,6 @@
 package com.mymacros.services.dao.entity;
 
 import com.mymacros.dto.entity.FoodRecipeDto;
-import com.mymacros.dto.entity.MacronutrientsDto;
 import com.mymacros.dto.entity.RecipeDto;
 
 import java.util.List;
@@ -9,7 +8,7 @@ import java.util.List;
 /**
  * Created by Tomas on 20/07/2016.
  */
-public interface RecipeServicesDao
+public interface RecipeServicesDao extends MacrosServicesDao
 {
      /**
       * =================================================>Recipe
@@ -88,43 +87,5 @@ public interface RecipeServicesDao
       * @param id Identificar que reprecenta el objeto a eliminar
       */
      void deleteFoodRecipe(long id);
-     /**
-      * =====================================================>Macros
-      */
-     /**
-      * <h1>addMacros</h1>
-      * <p>Agrega un elemento a la base de datos </p>
-      * @param macronutrientsDto Objeto que encapsula los nuevos elemetos
-      */
-     void addMacros(MacronutrientsDto macronutrientsDto);
 
-     /**
-      * <h1>getMacros</h1>
-      * <p>Obtiene un objeto a partir de su indetificador que lo relaciona</p>
-      * @param id Indetificador que reprecenta la relacion del objeto a quien lo esta llamado
-      * @return Retorna el objeto que se encuentra relacionad
-      */
-     MacronutrientsDto getMacros(long id);
-
-     /**
-      * <h1>getAllMactos</h1>
-      * <p>Obtiene una lista de elementos apartir del indetificador que los relaciona</p>
-      * @param id Indetificador que reprecenta la relacion de los objetos a buscar.
-      * @return Retorna una lista de elemetos.
-      */
-     List<MacronutrientsDto> getAllMacros(long id);
-
-     /**
-      * <h1>updateMactos</h1>
-      * <p>Actualiza un elemto de la base de datos</p>
-      * @param foodRecipeDto Objeto que encapsula los nuevos elemetos
-      */
-     void updateMacros(MacronutrientsDto foodRecipeDto);
-
-     /**
-      * <h1>deleteMacros</h1>
-      * <p>Da la orden al repositorio para eliminar un objeto</p>
-      * @param id Indetificador que reprecenta el obejto a eliminar.
-      */
-     void deleteMacros(long id);
 }
