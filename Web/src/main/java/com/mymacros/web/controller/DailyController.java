6f1346f4f9ba094f02b799f4dcc8df733c595d6d
tomas-93 <tomasyussef@gmail.com>
2016-07-25 01:35:07 -0500

    Se agregan los archivos de Daily de la vista y del controlador

---
+++ Web/src/main/java/com/mymacros/web/controller/DailyController.java
@@ -0,0 +1,19 @@
+package com.mymacros.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Tomas on 24/07/2016.
+ */
+@Controller
+@RequestMapping("daily")
+public class DailyController
+{
+     @RequestMapping(value = "", method = RequestMethod.GET)
+     public String getDaily()
+     {
+          return "daily/list";
+     }
+}
---
+++ Web/web/WEB-INF/jsp/view/daily/list.jsp
@@ -0,0 +1,14 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Tomas
+  Date: 24/07/2016
+  Time: 08:06 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ include file="/WEB-INF/jsp/base.jspf" %>
+<template:main htmlTitle="Diario" bodyTitle="Diario">
+    <jsp:body>
+        <h1>Diario</h1>
+    </jsp:body>
+</template:main>
