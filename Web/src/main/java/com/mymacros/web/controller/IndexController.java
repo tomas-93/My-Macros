518d2228bc66039a82bf8666d13a1d2d1fcf743b
tomas-93 <tomasyussef@gmail.com>
2016-07-26 16:12:50 -0500

    Al controlador Index ->Se le agrega la verificacion de la session, si no hay session se redirecciona a la vista de login

--- Web/src/main/java/com/mymacros/web/controller/IndexController.java
+++ Web/src/main/java/com/mymacros/web/controller/IndexController.java
@@ -5,14 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by Tomas on 23/07/2016.
  */
 @Controller
 public class IndexController
 {
+     @RequestMapping(value = {"/daily/*", "/food/*", "/profile/*"})
+     public void getIndex(HttpServletRequest request, HttpServletResponse response) throws IOException
+     {
+          if (request.getSession().getAttribute("idUser") == null)
+               response.sendRedirect("/user/login");
+     }
      @RequestMapping("/")
-     public View getIndex()
+     public View getIdenx()
      {
           return new RedirectView("user/login");
      }
