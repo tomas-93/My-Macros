6c47a070332246ba8fbcf8be48885ba2935861d9
tomas-93 <tomasyussef@gmail.com>
2016-08-10 18:32:17 -0500

    JS de todas las paginas JSP

--- Web/web/resource/js/app.js
+++ Web/web/resource/js/app.js
@@ -10,3 +10,56 @@ function card()
     });
 
 }
+
+function rage(id)
+{
+    console.log(id);
+    if(id == "rageCarb")
+    {
+        $(".totalCarbohydrates").val($("#rageCarb").val());
+
+    }
+
+    if(id == "rageFiber")
+    {
+        $(".fiber").val($("#rageFiber").val());
+    }
+
+    if(id == "rageProte")
+    {
+        $(".totalProtein").val($("#rageProte").val());
+    }
+
+    if(id == "rageCal")
+    {
+        $(".totalCal").val($("#rageCal").val());
+    }
+
+    if(id == "rageFat")
+    {
+        $(".totalFats").val($("#rageFat").val());
+    }
+}
+
+function sw()
+{
+    if ($("#switch1").val() == "on")
+    {
+        $(".x").css("display","none");
+        $(".polyunsaturated").val(0);
+        $(".monounsaturated").val(0);
+        $(".saturated").val(0);
+        $(".sugar").val(0);
+        $("#switch1").val("off");
+    }
+    else
+    {
+        $(".x").css("display","inline-block");
+        $(".polyunsaturated").val("");
+        $(".monounsaturated").val("");
+        $(".saturated").val("");
+        $(".sugar").val("");
+        $("#switch1").val("on");
+    }
+
+}
